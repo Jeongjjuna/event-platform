f54b7a1dd96f53e080a68d7e89c7a8d5ad1fe9e6
jeongjihun <wlgns2234@naver.com>
2026-07-08 00:26:05 +0900

    init: auth-api 프로젝트 세팅

---
+++ auth-api/src/test/kotlin/yjh/ontongsal/authapi/AuthApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package yjh.ontongsal.authapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
